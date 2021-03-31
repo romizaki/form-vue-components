5ebf50791ff0c993c6a94a9ccc44b8fe6dc84140
romizaki <rzferdiyanto@gmail.com>
2021-03-31 19:33:32 +0700

    error handling testing button

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,6 +2,7 @@ import { shallowMount, mount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 import Display from '../../src/components/Display.vue'
 import RadioButton from '../../src/components/RadioButton.vue'
+import sinon from 'sinon'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
@@ -14,18 +15,26 @@ describe('HelloWorld.vue', () => {
 })
 
 describe('Display.vue', () => {
-  it('Should render element correctly', () => {
-    const wrapper = mount(Display)
+  it('Should render text correctly', async () => {
+    const spy = sinon.spy()
+    const wrapper = mount(Display, {
+      propsData: {
+        textField: spy
+      }
+    })
     expect(wrapper.text()).toContain('Radio Button 1')
     expect(wrapper.text()).toContain('Radio Button 2')
     expect(wrapper.text()).toContain('Radio Button 3')
+    await wrapper.find('button').trigger('click')
+    // console.log(spy())
+    // spy.should.have.been.calledWidth('submit')
   })
 })
 
 describe('RadioButton.vue', () => {
   it('Should render element correctly', () => {
     const wrapper = shallowMount(RadioButton)
-    console.log(wrapper)
-    // expect(wrapper.text())
+    console.log(wrapper.text(), '<<<<')
+    expect(wrapper.text())
   })
 })
